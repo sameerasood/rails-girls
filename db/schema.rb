27205ebeaf12f6dff2d6e8a5c236882eeb9d697e
sameera sood <sameerasood@gmail.com>
2022-11-28 12:31:59 +0000

    Add comments to the idea

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_130143) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_120342) do
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
